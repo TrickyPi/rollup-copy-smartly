f1951093c5867587838def662994406b0af99ebf
TrickyPi <530257315@qq.com>
2022-02-14 13:56:57 +0800

    test: add rollup build test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,4 @@
-import fs from "fs";
-import { access } from "fs/promises";
+import fse from "fs-extra";
 import { resolve } from "path";
 import {
   watch,
@@ -7,70 +6,71 @@ import {
   RollupOptions,
   RollupWatcher,
   RollupWatcherEvent,
+  OutputOptions,
 } from "rollup";
-import { test, describe, expect, beforeAll, afterAll, afterEach } from "vitest";
+import { test, describe, expect, afterEach } from "vitest";
 import copy from "../src/index";
+
+//copy from https://github.com/rollup/rollup/blob/master/test/watch/index.js#L37
+interface SequenceFunction {
+  (event?: RollupWatcherEvent): void;
+}
+type SequenceEvents = (RollupWatcherEvent["code"] | SequenceFunction)[];
+async function sequence(
+  watcher: RollupWatcher,
+  events: SequenceEvents,
+  timeout = 300
+) {
+  function wait(timeout = 100) {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, timeout);
+    });
+  }
+  await new Promise<void>((fulfil, reject) => {
+    function go(event?: RollupWatcherEvent) {
+      const next = events.shift();
+      if (!next) {
+        watcher.close();
+        fulfil();
+      } else if (typeof next === "string") {
+        watcher.once("event", (event) => {
+          if (event.code !== next) {
+            watcher.close();
+            if (event.code === "ERROR") console.log(event.error);
+            reject(new Error(`Expected ${next} event, got ${event.code}`));
+          } else {
+            go(event);
+          }
+        });
+      } else {
+        Promise.resolve()
+          .then(() => wait(timeout)) // gah, this appears to be necessary to fix random errors
+          .then(() => next(event))
+          .then(() => go())
+          .catch((error) => {
+            watcher.close();
+            reject(error);
+          });
+      }
+    }
+    go();
+  });
+  return wait(100);
+}
 
 const rollConfig: RollupOptions = {
   input: "__tests__/fixture/src/entry.js",
   output: {
     dir: "__tests__/fixture/dist/",
     format: "cjs",
-    exports: "auto",
   },
 };
 
 describe("test", async () => {
-  //copy from https://github.com/rollup/rollup/blob/master/test/watch/index.js#L37
-  interface SequenceFunction {
-    (event?: RollupWatcherEvent): void;
-  }
-  type SequenceEvents = (RollupWatcherEvent["code"] | SequenceFunction)[];
-  async function sequence(
-    watcher: RollupWatcher,
-    events: SequenceEvents,
-    timeout = 300
-  ) {
-    function wait(timeout = 100) {
-      return new Promise<void>((resolve) => {
-        setTimeout(() => {
-          resolve();
-        }, timeout);
-      });
-    }
-    await new Promise<void>((fulfil, reject) => {
-      function go(event?: RollupWatcherEvent) {
-        const next = events.shift();
-        if (!next) {
-          watcher.close();
-          fulfil();
-        } else if (typeof next === "string") {
-          watcher.once("event", (event) => {
-            if (event.code !== next) {
-              watcher.close();
-              if (event.code === "ERROR") console.log(event.error);
-              reject(new Error(`Expected ${next} event, got ${event.code}`));
-            } else {
-              go(event);
-            }
-          });
-        } else {
-          Promise.resolve()
-            .then(() => wait(timeout)) // gah, this appears to be necessary to fix random errors
-            .then(() => next(event))
-            .then(() => go())
-            .catch((error) => {
-              watcher.close();
-              reject(error);
-            });
-        }
-      }
-      go();
-    });
-    return wait(100);
-  }
   afterEach(() => {
-    fs.rmSync(resolve("__tests__/fixture/dist"), {
+    fse.rmSync(resolve("__tests__/fixture/dist"), {
       force: true,
       recursive: true,
     });
@@ -78,7 +78,7 @@ describe("test", async () => {
   test("update file", async () => {
     const file = resolve("__tests__/fixture/src/template.html");
     const dest = resolve("__tests__/fixture/dist/template.html");
-    fs.writeFileSync(file, "");
+    fse.writeFileSync(file, "");
     const copyPlugin = copy({
       targets: [
         {
@@ -87,8 +87,7 @@ describe("test", async () => {
         },
       ],
     });
-    rollConfig.plugins ??= [];
-    rollConfig.plugins.push(copyPlugin);
+    rollConfig.plugins = [copyPlugin];
     const watcher = watch(rollConfig);
     await sequence(watcher, [
       "START",
@@ -96,17 +95,40 @@ describe("test", async () => {
       "BUNDLE_END",
       "END",
       () => {
-        fs.writeFileSync(file, "div");
+        fse.writeFileSync(file, "div");
       },
       "START",
       "BUNDLE_START",
       "BUNDLE_END",
       "END",
       () => {
-        const file = fs.readFileSync(dest, "utf-8");
+        const file = fse.readFileSync(dest, "utf-8");
         expect(file).toEqual("div");
       },
     ]);
-    fs.rmSync(file);
+    fse.rmSync(file);
+  });
+  test("rollup build", async () => {
+    const file = resolve("__tests__/fixture/src/template.html");
+    const dest = resolve("__tests__/fixture/dist/template.html");
+    fse.writeFileSync(file, "build");
+    const copyPlugin = copy({
+      targets: [
+        {
+          file: file,
+          dest: dest,
+        },
+      ],
+    });
+    const { input, output: outputOption } = rollConfig;
+    const inputOptions = {
+      input,
+      plugins: [copyPlugin],
+    };
+    const bundle = await rollup(inputOptions);
+    await bundle.write(outputOption as OutputOptions);
+    await bundle.close();
+    expect(fse.readFileSync(dest, "utf-8")).toEqual("build");
+    fse.rmSync(file);
   });
 });
